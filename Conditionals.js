6523ab65b488cfeadfe93dbcf77987e18b3d6c8f
Bungcwalisa <magobiyanebungcwalisa.com>
2025-10-31 12:16:49 +0200

    Conditionals

---
+++ Conditionals.js
@@ -0,0 +1,14 @@
+score = 90
+passingGrade = 60
+
+if (score >= passingGrade){
+    console.log("You Passed! Well done.");
+ } else {(score <= passingGrade)
+        console.log("Try harder next time!");
+}
+
+if (score === 100) {
+    console.log("You Passed! Well done.");
+} else {
+    console.log("Keep going!");
+}
